3719f8eaad60660d9f3d1dd8eff274d92c20602f
johnny6464 <johnny19641964@gmail.com>
2018-12-01 10:06:18 +0800

    new add

---
+++ app.py
@@ -0,0 +1,82 @@
+import random
+import configparser
+import os
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, LocationSendMessage, SourceUser
+)
+
+app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# config
+line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
+handler = WebhookHandler(config['line_bot']['Channel_Secret'])
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except:
+        print("something wrong")
+    return 'OK'
+
+
+@app.route("/push", methods=['GET'])
+def pushmessage():
+    for uid in users:
+        line_bot_api.push_message(uid, TextSendMessage(text='Hello World'))
+    return "push user# " + str(len(users)) + " OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+    text = event.message.text
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            global users
+            users.append(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.source.user_id)
+            )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    print("package_id:", event.message.package_id)
+    print("sticker_id:", event.message.sticker_id)
+    sticker_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 100, 101, 102, 103, 104, 105, 106,
+                   107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
+                   126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 401, 402]
+    index_id = random.randint(0, len(sticker_ids) - 1)
+    sticker_id = str(sticker_ids[index_id])
+    sticker_message = StickerSendMessage(package_id='1', sticker_id=sticker_id)
+    line_bot_api.reply_message(event.reply_token, sticker_message)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
